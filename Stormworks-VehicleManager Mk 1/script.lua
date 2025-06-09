bfb61d5f4cc689771bb21a202ca6c2b6d7a2483a
Bw-Chan <137831599+Bw-Chan@users.noreply.github.com>
2025-06-09 18:58:48 +0100

    Updated vehicle manager to stable version

--- Stormworks-VehicleManager Mk 1/script.lua
+++ Stormworks-VehicleManager Mk 1/script.lua
@@ -13,52 +13,50 @@ g_savedata = {
     ["Manager_Name"] = "BW-VM mk1", --For vehicle info popups/UI
     ["MaxVehiclesAllowed"] = 1,
     ["Default_PVP_Value"] = false,
-    ["Default_AS_Value"] = true
+    ["Default_AS_Value"] = true,
+    ["noWorkshop"] = true
 }
 
 
 --# Simplified functions best if you dont change these #--
-function toggleAS(peer_id,state)
-	for a,peer in pairs(g_savedata["players"]) do
-		if peer[2] == peer_id then
-			g_savedata["players"][a][4] = state
-			for b,group_id in pairs(peer[3]) do
-				VEHICLES = server.getVehicleGroup(group_id)
-				for c,vehicle_id in pairs(VEHICLES) do
-					server.setVehicleEditable(vehicle_id, not state)
-				end
-			end
-			server.notify(peer_id, "[ANTI-STEAL]", "Toggled State: "..tostring(state), 8)
+function toggleAS(index,peer_id,state)
+	g_savedata["players"][index][4] = state
+	for b,group_id in pairs(g_savedata["players"][index][3]) do
+		VEHICLES = server.getVehicleGroup(group_id)
+		for c,vehicle_id in pairs(VEHICLES) do
+			server.setVehicleEditable(vehicle_id, not state)
 		end
 	end
+	server.notify(peer_id, "[ANTI-STEAL]", "Toggled State: "..tostring(state), 8)
 end
 --function to toggle the pvp
-function togglePVP(peer_id,state)
-	for a,peer in pairs(g_savedata["players"]) do
-		if peer[2] == peer_id then
-			g_savedata["players"][a][5] = state
-			for b,group_id in pairs(peer[3]) do
-				VEHICLES = server.getVehicleGroup(group_id)
-				for c,vehicle_id in pairs(VEHICLES) do
-					server.setVehicleInvulnerable(vehicle_id, not state)
-				end
-			end
-			server.notify(peer_id, "[PVP]", "Toggled State: "..tostring(state), 8)
+function togglePVP(index,peer_id,state)
+	g_savedata["players"][index][5] = state --updates the pvp state of the userdata table
+	for b,group_id in pairs(g_savedata["players"][index][3]) do
+		VEHICLES = server.getVehicleGroup(group_id)
+		for c,vehicle_id in pairs(VEHICLES) do
+			server.setVehicleInvulnerable(vehicle_id,not state)
 		end
 	end
+	server.notify(peer_id, "[PVP]", "Toggled State: "..tostring(state), 8)
 end
 --function to toggle the UI
-function toggleUI(peer_id,state)
-	for a,peer in pairs(g_savedata["players"]) do
-		if peer[2] == peer_id then
-			g_savedata["players"][a][6] = state
-			server.notify(peer_id, "[UI]", "Toggled State: "..tostring(state), 8)
-		end
-	end
+function toggleUI(index,peer_id,state)
+	g_savedata["players"][index][6] = state
+	server.notify(peer_id, "[UI]", "Toggled State: "..tostring(state), 8)
 end
 
 --#      MAIN SCRIPT        #--
 -- Change at your discretion --
+
+function obtainSteamID(peer_id)
+    local list = server.getPlayers()
+    for _,o in pairs(list) do
+        if peer_id == o["id"] then
+            return o["steam_id"]
+        end
+    end
+end
 
 --Adds player to g_savedata and sets up their defaults
 function onPlayerJoin(steam_id, name, peer_id, is_admin, is_auth) --Adds player to g_savedata players for easier reload
@@ -66,6 +64,7 @@ function onPlayerJoin(steam_id, name, peer_id, is_admin, is_auth) --Adds player 
 	local PVP = g_savedata["Default_PVP_Value"]
 	local UI = true --always true (player can toggle this off)
 	table.insert(g_savedata["players"],1,{name,peer_id,{},AntiSteal,PVP,UI})
+	
 end
 --Removed the player from g_savedate
 function onPlayerLeave(steam_id, name, peer_id, is_admin, is_auth) --Removes player from g_savedata players
@@ -88,6 +87,7 @@ function onGroupSpawn(group_id, peer_id,x,y,z,cost)
 	local players = g_savedata["players"]
 	local GroupList = players[3]
 	local name = server.getPlayerName(peer_id)
+    local delete = false
 	for _,i in pairs(players) do
 		if i[2] == peer_id then
 			table.insert(g_savedata["players"][_][3],1,tostring(group_id))
@@ -99,6 +99,15 @@ function onGroupSpawn(group_id, peer_id,x,y,z,cost)
 			server.notify(i[2], g_savedata["Manager_Name"], "Spawned Vehicle ID: \n"..group_id, 5)
 		end
 	end
+	--Should limit the vehicles spawned by the player
+	for _,i in pairs(g_savedata["players"]) do
+		local VehicleCount = #i[3] --gets the length of the vehicles in a player's data
+		if VehicleCount > g_savedata["MaxVehiclesAllowed"] then --does a comparison
+			server.despawnVehicleGroup(i[3][#i[3]],true)
+			table.remove(g_savedata["players"][_][3],#i[3])
+		end
+	end
+	
 end
 -- Removes the vehicle group from a player's vehicle table
 -- Also quick note why the fuck is there no onGroupDespawn? Would of made things a little simpler
@@ -115,50 +124,53 @@ function onVehicleDespawn(vehicle_id, peer_id)
 			end
 		end
 	end
+	
 end
 
--- This functions as a UI updater
+--this function updates the UI
+function updateUI(peer_id)
+	
+	--obtain information
+	for _,i in pairs(g_savedata["players"]) do
+		if peer_id == i[2] then
+			peer_id = i[2]
+			vehicles = i[3]
+			UI = i[6] --gets UI state
+			PVP = i[5] --gets PVP state
+			AntiSteal = i[4] --gets AntiSteal state
+		end
+	end
+	
+	if UI then
+		local vehs=""
+		for a,j in pairs(vehicles) do
+			vehs = vehs..tostring(j).."\n"
+		end
+		local as=tostring(AntiSteal)
+		local pvp=tostring(PVP)
+		local Line1= string.format("%s\n",g_savedata["Manager_Name"])
+		local Line2= "Anti-Steal: "..as.."\n"
+		local LineDASH= "-------------\n"
+		local Line3= "PvP: "..pvp.."\n"
+		local Line4= "--Vehicles--\n"..vehs
+		
+		-- Creates and Updates status Popup with id = 1 for peer_id (exclusive and will not affect other UI for players)
+		server.setPopupScreen(peer_id, 1, name, true, Line1..Line2..LineDASH..Line3..Line4,0.9,0.4)
+	else
+		server.removePopup(peer_id, 1) --Removes the Vehicle Manager UI for the peer_id
+	end
+end
+
 -- and makes sure you dont die with pvp off and
 -- Makes sure you dont go over the set amount of vehicles
 function onTick(game_ticks)
-	for _,i in pairs(g_savedata["players"]) do
-		local AntiSteal = i[4] --Get Anti-Steal state from a player in DB
-		local PVP = i[5] --Get PVP state from a player in DB
-		local UI = i[6] --Get UI state from player in DB
-		
-		if i[5] == false then
-			character_id = server.getPlayerCharacterID(i[2])
+	for index,item in pairs(g_savedata["players"]) do
+		if item[5] == false then
+			local character_id = server.getPlayerCharacterID(item[2])
 			server.reviveCharacter(character_id)
 			server.setCharacterData(character_id, 100, true, false)
 		end
-
-        --
-		if UI then
-			vehs=""
-			for a,j in pairs(i[3]) do
-				vehs = vehs.. "\n" .. tostring(j)
-			end
-			as=tostring(AntiSteal)
-			pvp=tostring(PVP)
-			Line1= string.format("%s\n",g_savedata["Manager_Name"])
-			Line2= "Anti-Steal: "..as.."\n"
-			LineDASH= "-------------\n"
-			Line3= "PvP: "..pvp.."\n"
-			Line4= "--Vehicles--\n"..vehs
-		
-			-- Creates and Updates popup for peer_id with id = peer_id+1 (id = 1 essentially)
-			server.setPopupScreen(i[2], 1, name, true, Line1..Line2..LineDASH..Line3..Line4,0.9,0.4)
-		else
-			server.removePopup(i[2], 1) --Removes the Vehicle Manager UI for the player
-		end
-	end
-	--Should limit the vehicles spawned by the player
-	for _,i in pairs(g_savedata["players"]) do
-		local VehicleCount = #i[3] --gets the length of the vehicles in a player's data
-		if VehicleCount > g_savedata["MaxVehiclesAllowed"] then --does a comparison
-			server.despawnVehicleGroup(i[3][#i[3]],true)
-			table.remove(g_savedata["players"][_][3],#i[3])
-		end
+        updateUI(item[2]) --updates UI for peer_id every tick (shockingly more reliable)
 	end
 end
 
@@ -187,6 +199,7 @@ function onCustomCommand(full_message, peer_id, is_admin, is_auth, command, one,
                         server.despawnVehicleGroup(one,true)
                         table.remove(g_savedata["players"][_][3],a)
                         server.notify(i[2], g_savedata["Manager_Name"], "Despawned Vehicle ID: \n"..one, 6)
+						break
                     else
                         server.despawnVehicleGroup(group_id,true)
                         table.remove(g_savedata["players"][_][3],a)
@@ -207,6 +220,7 @@ function onCustomCommand(full_message, peer_id, is_admin, is_auth, command, one,
                         for b,vehicle_id in pairs(one) do
                             server.resetVehicleState(vehicle_id)
                         end
+						break
                     else
                         for b,vehicle_id in pairs(VEHICLES) do
                             server.resetVehicleState(vehicle_id) --reset each vehicle in group
@@ -237,8 +251,8 @@ function onCustomCommand(full_message, peer_id, is_admin, is_auth, command, one,
                             x,y,z = matrix.position(VehicleMatrix)
                             --sets the translation of the vehicle_id by x y z
                             server.setVehiclePos(tonumber(vehicle_id), matrix.translation(x,y+1,z))
-                            break
                         end
+						break
                     else
                         --goes through each vehicle flipping them
                         for b,vehicle_id in pairs(VEHICLES) do
@@ -257,7 +271,7 @@ function onCustomCommand(full_message, peer_id, is_admin, is_auth, command, one,
     end
         
     --Player TP_Vehicle Command
-    if (command == "?tpveh" or command == "?tp_vehicle") then
+    if (command == "?tpv" or command == "?tp_vehicle" or command == "?tpveh") then
         player_matrix = server.getPlayerPos(peer_id)
         x,y,z = matrix.position(player_matrix)
         for _,i in pairs(players) do
@@ -266,11 +280,13 @@ function onCustomCommand(full_message, peer_id, is_admin, is_auth, command, one,
                     if one then
                         --server.moveGroup(tonumber(one), matrix.translation(x,y+10,z))
                         server.setGroupPos(tonumber(one), matrix.translation(x,y+10,z))
+						break
                     else
                         --server.moveGroup(tonumber(group_id), matrix.translation(x,y+10,z))
                         server.setGroupPos(tonumber(group_id), matrix.translation(x,y+10,z))
                     end
                 end
+				break
             end
         end
     end
@@ -279,34 +295,38 @@ function onCustomCommand(full_message, peer_id, is_admin, is_auth, command, one,
         
     --Player PvP Toggle
     if (command=="?pvp") then
-        for _,i in pairs(players) do
-            if i[2] == peer_id then
-                togglePVP(peer_id,not i[5])
+        for index,item in pairs(players) do
+            if item[2] == peer_id then
+                togglePVP(index,peer_id,not item[5])
+                break
             end
-            end
+        end
     end
         
     --Player Anti-steal Toggle
     if (command=="?as" or command=="?anti_steal") then
-        for _,i in pairs(players) do
-            if i[2] == peer_id then
-                toggleAS(peer_id,not i[4])
+        for index,item in pairs(players) do
+            if item[2] == peer_id then
+                toggleAS(index,peer_id,not item[4])
+                break
             end
         end
     end
         
     --Player UI Toggle
     if (command=="?ui") then
-        for _,i in pairs(players) do
-            if i[2] == peer_id then
-                toggleUI(peer_id,not i[6])
+        for index,item in pairs(players) do
+            if item[2] == peer_id then
+                toggleUI(index,peer_id,not item[6])
+                break
             end
         end
     end
         
     --ADMIN VEHICLE COMMANDS--
     --Displays a list of players and their vehicles
-    if (command == "?vehlist") and is_admin then
+    if is_admin then
+    if (command == "?vehlist") then
         plistT = ""
         for _,i in pairs(players) do
             VLPP = ""
@@ -317,13 +337,14 @@ function onCustomCommand(full_message, peer_id, is_admin, is_auth, command, one,
         end
         server.announce("[Admin Tools]", plistT, (peer_id))
     end
+
     --Sets the pvp of those naughty scoundrels
     if (command == "?setpvp") and is_admin then
         if one and two then --One = target_player Two = state
             for _,i in pairs(players) do
                 if i[2] == tonumber(one) then
                     local tobool={ ["true"]=true, ["false"]=false }
-                    togglePVP(tonumber(one),tobool[two:lower()])
+                    togglePVP(_,tonumber(one),tobool[two:lower()])
                 end
             end
         else
@@ -331,20 +352,45 @@ function onCustomCommand(full_message, peer_id, is_admin, is_auth, command, one,
         end
     end
         
-    if (command == "?remove") and is_admin then
+    if (command == "?remove") then
         if one then
             for _,i in pairs(players) do
                 for a,group_id in pairs(players[_][3]) do
                     if group_id == tonumber(one) then
                         server.announce("[Admin Tools]","Attempting to remove vehicle"..group_id,(peer_id))
-                        table.remove(g_savedata["players"][_][3],a)
+						is_success = server.despawnVehicleGroup(tonumber(one), true)
+						if is_success then
+                        	table.remove(g_savedata["players"][_][3],a)
+
+							break
+						end
                     end
                 end
             end
-            server.despawnVehicleGroup(tonumber(one), true)
+            
             server.announce("[Admin Tools]","Removed Vehicle: "..one,(peer_id))
         else
             server.announce("[Admin Tools]","You need a Vehicle ID",(peer_id))
         end
+    end
+
+    if (command == "?dg_print") then
+        for _,i in pairs(g_savedata["players"]) do
+            local sep = " / "
+            local name = tostring(i[1])
+            local peer_id = tostring(i[2])
+            server.announce("[Debug]",name..sep..peer_id,(peer_id))
+			local as=tostring(i[4])
+			local pvp=tostring(i[5])
+            local ui=tostring(i[6])
+            server.announce("[Debug]",as..sep..pvp..sep..ui,(peer_id))
+            local vehs=""
+			for a,j in pairs(i[3]) do
+				vehs = vehs..tostring(j).."\n"
+			end
+            server.announce("[Debug]",vehs,(peer_id))
+        end
+    end
+
     end
 end					
